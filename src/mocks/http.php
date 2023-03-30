abfae06d406242a4e5b8ae8ab06469cf79586462
Jordan Pakrosnis <jpak@jordanpak.com>
2023-03-30 11:44:04 -0400

    Add periods to method doc param/return comments

--- src/mocks/http.php
+++ src/mocks/http.php
@@ -214,11 +214,11 @@ class Http implements \PMC\Unit_Test\Interfaces\Mocker {
 	 *
 	 * @see Requests_Transport_cURL::request() for details
 	 *
-	 * @param  string       $url      URL to request
-	 * @param  array        $headers  Associative array of request headers
-	 * @param  string|array $data     Data to send either as the POST body, or as parameters in the URL for a GET/HEAD
-	 * @param  array        $options  Request options, see {@see Requests::response()} for documentation
-	 * @return string                 Raw HTTP result
+	 * @param  string       $url      URL to request.
+	 * @param  array        $headers  Associative array of request headers.
+	 * @param  string|array $data     Data to send either as the POST body, or as parameters in the URL for a GET/HEAD.
+	 * @param  array        $options  Request options, see {@see Requests::response()} for documentation.
+	 * @return string                 Raw HTTP result.
 	 */
 	public function request( $url, $headers = [], $data = [], $options = [] ) {
 
