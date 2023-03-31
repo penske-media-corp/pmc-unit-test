6aa17fd428b3d3d0ee008c4638d9b47ddcfd30ec
Jordan Pakrosnis <jpak@jordanpak.com>
2023-03-31 15:19:30 -0400

    Add 6.1+ compatibility to expectedDeprecated

--- src/traits/base.php
+++ src/traits/base.php
@@ -474,24 +474,46 @@ trait Base {
 	}
 
 	/**
-	 * Overload function to allow manual bypass deprecated errors
+	 * Overload function to allow manual bypass deprecated errors\
+	 *
 	 * @codeCoverageIgnore Don't have a good way to trigger the code coverage at the moment
 	 */
 	public function expectedDeprecated() {  // phpcs:ignore
 		$caught_deprecated = [];
-		foreach ( $this->caught_deprecated as $caught ) {
-			$caught = apply_filters( 'pmc_deprecated_function', $caught );
+		foreach ( $this->caught_deprecated as $caught => $description ) {
+			if ( is_numeric( $caught ) ) {
+				$caught      = $description;
+				$description = false;
+			}
+			$caught = apply_filters( 'pmc_deprecated_function', $caught, $description );
 			if ( ! empty( $caught ) ) {
-				$caught_deprecated[] = $caught;
+				if ( $description === false ) {
+					/* WP 6.0 */
+					$caught_deprecated[] = $caught;
+				}
+				else {
+					/* WP 6.1 */
+					$caught_deprecated[$caught] = $description;
+				}
 			}
 		}
 		$this->caught_deprecated = $caught_deprecated;
-
 		$caught_doing_it_wrong = [];
-		foreach ( $this->caught_doing_it_wrong as $caught ) {
-			$caught = apply_filters( 'pmc_doing_it_wrong', $caught );
+		foreach ( $this->caught_doing_it_wrong as $caught => $description ) {
+			if ( is_numeric( $caught ) ) {
+				$caught      = $description;
+				$description = false;
+			}
+			$caught = apply_filters( 'pmc_doing_it_wrong', $caught, $description );
 			if ( ! empty( $caught ) ) {
-				$caught_doing_it_wrong[] = $caught;
+				if ( $description === false ) {
+					/* WP 6.0 */
+					$caught_doing_it_wrong[] = $caught;
+				}
+				else {
+					/* WP 6.1 */
+					$caught_doing_it_wrong[$caught] = $description;
+				}
 			}
 		}
 		$this->caught_doing_it_wrong = $caught_doing_it_wrong;
